16cfd41f6b8fee728db13a03f53f5bd5a895c0e6
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2023-01-16 18:22:13 +0100

    refactor: rename data-testid attribute

--- cypress/e2e/adding_a_restaurant.cy.ts
+++ cypress/e2e/adding_a_restaurant.cy.ts
@@ -3,9 +3,9 @@ describe('adding a restaurant', () => {
     const restaurantName = 'Oshi Sushi'
 
     cy.visit('http://localhost:5173') // Go to Homepage
-    cy.get('[data-test="addRestaurantButton"]').click() // Click on the Add Restaurant Button
-    cy.get('[data-test="newRetaurantName"]').type(restaurantName) // Enter a Restaurant name in the input
-    cy.get('[data-test="saveNewRestaurantButton"]').click() // Click on the Save Restaurant Button
+    cy.get('[data-testid="addRestaurantButton"]').click() // Click on the Add Restaurant Button
+    cy.get('[data-testid="newRetaurantName"]').type(restaurantName) // Enter a Restaurant name in the input
+    cy.get('[data-testid="saveNewRestaurantButton"]').click() // Click on the Save Restaurant Button
 
     cy.contains(restaurantName) // Expect the new Restaurant name is on the page
   })
