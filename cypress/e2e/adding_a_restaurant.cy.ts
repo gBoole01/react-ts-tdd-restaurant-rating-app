d26b42e4670d08df4ef136d98f02a439530ff144
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2023-01-17 09:50:18 +0100

    test: add some assertions on e2e test

--- cypress/e2e/adding_a_restaurant.cy.ts
+++ cypress/e2e/adding_a_restaurant.cy.ts
@@ -3,9 +3,12 @@ describe('adding a restaurant', () => {
     const restaurantName = 'Oshi Sushi'
 
     cy.visit('http://localhost:5173') // Go to Homepage
+    cy.get('[data-testid="addRestaurantButton"]').should('exist') // Check that the Add Restaurant Button is shown
+    cy.get('[data-testid="newRetaurantName"]').should('not.exist') // Check that the Input field not shown
     cy.get('[data-testid="addRestaurantButton"]').click() // Click on the Add Restaurant Button
     cy.get('[data-testid="newRetaurantName"]').type(restaurantName) // Enter a Restaurant name in the input
     cy.get('[data-testid="saveNewRestaurantButton"]').click() // Click on the Save Restaurant Button
+    cy.get('[data-testid="newRetaurantName"]').should('not.exist') // Check that the Input field not shown
 
     cy.contains(restaurantName) // Expect the new Restaurant name is on the page
   })
