5499b7c95f4b71673aae850eab7ff0844a552682
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2023-01-18 15:09:52 +0100

    fix: typo in newRestaurantName

--- cypress/e2e/adding_a_restaurant.cy.ts
+++ cypress/e2e/adding_a_restaurant.cy.ts
@@ -1,33 +1,33 @@
-function shownElementsArePresentAtTheStart() {
-  cy.get('[data-testid="addRestaurantButton"]').should('exist')
-}
-
-function hiddenElementsAreNotPresentAtTheStart() {
-  cy.get('[data-testid="newRetaurantName"]').should('not.exist')
-}
-
-function modalCanBeCancelled() {
-  cy.get('[data-testid="addRestaurantButton"]').click()
-  cy.get('[data-testid="closeNewRestaurantModalButton"]').click()
-  cy.get('[data-testid="newRetaurantName"]').should('not.exist')
-}
-
-function modalDisplaysValidationErrors() {
-  cy.get('[data-testid="addRestaurantButton"]').click()
-  cy.get('[data-testid="saveNewRestaurantButton"]').click()
-  cy.get('[data-testid="newRetaurantNameError"]').contains('Cannot be blank')
-  cy.get('[data-testid="closeNewRestaurantModalButton"]').click()
-}
-
-function modalAllowsAddingARestaurant(restaurantName: string) {
-  cy.get('[data-testid="addRestaurantButton"]').click()
-  cy.get('[data-testid="newRetaurantName"]').type(restaurantName)
-  cy.get('[data-testid="saveNewRestaurantButton"]').click()
-  cy.get('[data-testid="newRetaurantName"]').should('not.exist')
-  cy.contains(restaurantName)
-}
-
 describe('adding a restaurant', () => {
+  function shownElementsArePresentAtTheStart() {
+    cy.get('[data-testid="addRestaurantButton"]').should('exist')
+  }
+
+  function hiddenElementsAreNotPresentAtTheStart() {
+    cy.get('[data-testid="newRestaurantName"]').should('not.exist')
+  }
+
+  function modalCanBeCancelled() {
+    cy.get('[data-testid="addRestaurantButton"]').click()
+    cy.get('[data-testid="closeNewRestaurantModalButton"]').click()
+    cy.get('[data-testid="newRestaurantName"]').should('not.exist')
+  }
+
+  function modalDisplaysValidationErrors() {
+    cy.get('[data-testid="addRestaurantButton"]').click()
+    cy.get('[data-testid="saveNewRestaurantButton"]').click()
+    cy.get('[data-testid="newRestaurantNameError"]').contains('Cannot be blank')
+    cy.get('[data-testid="closeNewRestaurantModalButton"]').click()
+  }
+
+  function addRestaurant(restaurantName: string) {
+    cy.get('[data-testid="addRestaurantButton"]').click()
+    cy.get('[data-testid="newRestaurantName"]').type(restaurantName)
+    cy.get('[data-testid="saveNewRestaurantButton"]').click()
+    cy.get('[data-testid="newRestaurantName"]').should('not.exist')
+    cy.contains(restaurantName)
+  }
+
   it('displays the restaurant in the list', () => {
     const restaurantName = 'Oshi Sushi'
 
@@ -37,6 +37,6 @@ describe('adding a restaurant', () => {
     hiddenElementsAreNotPresentAtTheStart()
     modalCanBeCancelled()
     modalDisplaysValidationErrors()
-    modalAllowsAddingARestaurant(restaurantName)
+    addRestaurant(restaurantName)
   })
 })
