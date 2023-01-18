0961761de2fc58f6e1244611fd020aa6a59528c3
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2023-01-18 11:15:24 +0100

    refactor: add Formik

--- src/components/NewRestaurantForm.tsx
+++ src/components/NewRestaurantForm.tsx
@@ -1,60 +1,90 @@
 import { Box, Button, TextField } from '@mui/material'
-import { useState } from 'react'
+import { useFormik } from 'formik'
 
 type NewRestaurantFormProps = {
   onSave: ({ name }: { name: string }) => void
   onCancel: () => void
 }
 
+type CloseModalButtonProps = {
+  onCancel: () => void
+}
+
+function CloseModalButton({ onCancel }: CloseModalButtonProps) {
+  return (
+    <Button
+      variant="outlined"
+      sx={{ width: '20%' }}
+      onClick={() => {
+        onCancel()
+      }}
+      type="button"
+      data-testid="closeNewRestaurantModalButton"
+    >
+      Cancel
+    </Button>
+  )
+}
+
+const inputProps = {
+  'data-testid': 'newRetaurantName',
+}
+
 export default function NewRestaurantForm({
   onSave,
   onCancel,
 }: NewRestaurantFormProps) {
-  const [name, setName] = useState('')
-
-  const inputProps = {
-    'data-testid': 'newRetaurantName',
-  }
+  const {
+    values,
+    touched,
+    errors,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    isSubmitting,
+  } = useFormik({
+    initialValues: {
+      name: '',
+    },
+    onSubmit: (data, { resetForm }) => {
+      onSave({ name: data.name })
+      resetForm({ values: { name: '' } })
+    },
+  })
 
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        gap: '.5rem',
-        width: '100%',
-      }}
-    >
-      <TextField
-        variant="outlined"
-        sx={{ width: '60%' }}
-        label="Name"
-        inputProps={inputProps}
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <Button
-        variant="outlined"
-        sx={{ width: '20%' }}
-        onClick={() => {
-          onCancel()
+    <form onSubmit={handleSubmit}>
+      <Box
+        sx={{
+          display: 'flex',
+          gap: '.5rem',
+          width: '100%',
         }}
-        type="button"
-        data-testid="closeNewRestaurantModalButton"
       >
-        Cancel
-      </Button>
-      <Button
-        variant="contained"
-        sx={{ width: '20%' }}
-        onClick={() => {
-          onSave({ name })
-          setName('')
-        }}
-        type="button"
-        data-testid="saveNewRestaurantButton"
-      >
-        Save
-      </Button>
-    </Box>
+        <TextField
+          variant="outlined"
+          sx={{ width: '60%' }}
+          label="Name"
+          name="name"
+          id="name"
+          inputProps={inputProps}
+          value={values.name}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          error={touched.name && Boolean(errors.name)}
+          helperText={touched.name && errors.name}
+        />
+        <CloseModalButton onCancel={onCancel} />
+        <Button
+          variant="contained"
+          sx={{ width: '20%' }}
+          type="submit"
+          disabled={isSubmitting}
+          data-testid="saveNewRestaurantButton"
+        >
+          Save
+        </Button>
+      </Box>
+    </form>
   )
 }
